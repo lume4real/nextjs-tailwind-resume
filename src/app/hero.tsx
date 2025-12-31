f7f076bf998b941e0a68ba75027d135bdf4ecc6f
EC2 Default User <ec2-user@ip-172-31-15-201.us-east-2.compute.internal>
2025-12-31 05:49:34 +0000

    adding files without node_modules and an updated next.config.js

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -4,45 +4,51 @@ import { IconButton, Typography } from "@material-tailwind/react";
 
 export default function Hero() {
   return (
-    <section className="relative w-full">
+    <div className="relative w-full">
       <div className="grid place-items-center min-h-[92vh] px-8">
         <div className="container mx-auto text-center">
-          <Typography variant="h1" color="blue-gray">
+
+          <Typography
+            as={"h1"}
+            variant={"h1"}
+            color={"blue-gray"}
+          >
             My Resume
           </Typography>
 
           <Typography
-            variant="lead"
-            color="gray"
-            className="mt-4 mb-12 mx-auto max-w-4xl"
+            as={"p"}
+            variant={"lead"}
+            color={"gray"}
+            className="mt-4 mb-12 w-full md:max-w-full lg:max-w-4xl"
           >
-            Welcome to my professional resume page! Below, you’ll find a
+            Welcome to my professional resume page! Below, you'll find a
             comprehensive overview of my skills, qualifications, and experience
             in the field of web development.
           </Typography>
 
           <Typography
-            variant="small"
+            as={"p"}
             className="mt-12 mb-4 text-blue-gray-900 font-medium uppercase"
           >
-            Connect with me
+            Connect me on:
           </Typography>
 
-          <div className="flex justify-center gap-2">
+          <div className="flex gap-2 justify-center">
             <IconButton variant="text" color="gray">
               <i className="fa-brands fa-facebook text-lg" />
             </IconButton>
-
             <IconButton variant="text" color="gray">
               <i className="fa-brands fa-instagram text-lg" />
             </IconButton>
-
             <IconButton variant="text" color="gray">
               <i className="fa-brands fa-github text-lg" />
             </IconButton>
           </div>
+
         </div>
       </div>
-    </section>
+    </div>
   );
 }
+
